3776ceecc55779bb89861e09d3d59c2cca8a9f63
ishan-marikar <ishan.marikar@outlook.com>
2015-04-25 02:02:08 +0530

    Removed dependency of json module and changed url

--- awwpaste_cli.py
+++ awwpaste_cli.py
@@ -1,24 +1,19 @@
 #!/usr/bin/env python
 
 import requests
-import json
 import sys
 import urllib
 
-url = "http://localhost:63342/AwwPasteV2/api/"
+url = "http://awwpaste.local/api/"
 
 
 def Paste( textToPaste ):
 	payload = {'text': textToPaste}
 	data =  requests.post(url, params = payload);
-	binary = data.content
-	response = json.loads(binary)
+	response = data.json()
 	return response
 	
 def Main():
-	#total = ""
-	#for line in sys.stdin:
-	#	total += line
 	keys = Paste(sys.stdin.read())
 	print "Your AwwPaste link is %s" % keys['pasteID']
 	print "Your AwwPaste delete link is %s" % keys['deleteKey']
