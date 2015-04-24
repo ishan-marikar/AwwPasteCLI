ca67fa8d56c25771b68a094a5bafc290347739ca
ishan-marikar <ishan.marikar@outlook.com>
2015-04-25 02:19:12 +0530

    Added API URL, URL formatting and output of stdin

--- awwpaste_cli.py
+++ awwpaste_cli.py
@@ -4,19 +4,23 @@ import requests
 import sys
 import urllib
 
-url = "http://awwpaste.local/api/"
+url = "http://awwpaste.local/"
+apiUrl = url + "api/"
 
 
 def Paste( textToPaste ):
 	payload = {'text': textToPaste}
-	data =  requests.post(url, params = payload);
+	data =  requests.post(apiUrl, params = payload);
 	response = data.json()
 	return response
 	
 def Main():
-	keys = Paste(sys.stdin.read())
-	print "Your AwwPaste link is %s" % keys['pasteID']
-	print "Your AwwPaste delete link is %s" % keys['deleteKey']
+	input =  sys.stdin.read()
+	print input
+	keys = Paste(input)
+	print "-" * 82
+	print "Your AwwPaste link is %s?id=%s" % ( url , keys['pasteID'] )
+	print "Your AwwPaste delete link is %s?id=%s&delete_key=%s" % ( url , keys['pasteID'], keys['deleteKey'] )
 	exit()
 
 if __name__ == '__main__':
